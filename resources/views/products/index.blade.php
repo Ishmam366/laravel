8ff6db046d5bd51e71ca9ebbfc7e5151d78b14ff
ishmam123 <ishmam212@gmail.com>
2023-12-19 11:43:50 +0600

    Your commit message here

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <h1>Product</h1>
+    <div>
+        @if(session()->has('success'))
+            <div>
+                {{session('success')}}
+            </div>
+        @endif
+    </div>
+    <div class="container">
+        <div type="button" class="btn btn-default">
+            <a href="{{route('product.create')}}">Create a Product</a>
+        </div>
+        <table class="table table-bordered">
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Qty</th>
+                <th>Price</th>
+                <th>Description</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
+            @foreach($products as $product )
+                <tr>
+                    <td>{{$product->id}}</td>
+                    <td>{{$product->name}}</td>
+                    <td>{{$product->qty}}</td>
+                    <td>{{$product->price}}</td>
+                    <td>{{$product->description}}</td>
+                    <td>
+                        <a href="{{route('product.edit', ['product' => $product])}}">Edit</a>
+                    </td>
+                    <td>
+                        <form method="post" action="{{route('product.destroy', ['product' => $product])}}">
+                            @csrf 
+                            @method('delete')
+                            <input type="submit" value="Delete" />
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
+</body>
+</html>
